f8cd7ddb07102863e20e0977c31f28b8a96a275a
Sudeep Silwal <officialsudeepsilwal@gmail.com>
2026-09-08 14:03:31 +0545

    og dimensions

--- src/app/page.js
+++ src/app/page.js
@@ -21,8 +21,8 @@ export const metadata = {
      images: [
     {
       url: '/og-image.png',
-      width: 1734,
-      height: 907,
+      width: 1200,
+      height: 630,
       alt: "Sudeep Silwal's Blog",
     },
   ],
